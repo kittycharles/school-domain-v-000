253248ba489883463883710cfea01edca615e57a
Richard Sanchez <richard.charles.s@gmail.com>
2018-07-18 19:15:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,11 @@ class School
     end
 
   def add_student(student, grade)
+    if @roster[grade] != nil
     @roster[grade] << [student]
-    # else
-    #   @roster[grade] << [student]
-    # end
+    else
+      @roster[grade] = [student]
+    end
   end
 #
 #
