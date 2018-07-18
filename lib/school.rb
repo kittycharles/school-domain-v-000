8d170881c94307c22ea761cad6ab29a5956b7645
Richard Sanchez <richard.charles.s@gmail.com>
2018-07-18 19:12:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+# # code here!
+class School
+  attr_reader :name
+  attr_accessor :roster
+
+  @roster = {}
+    
+  def initialize(name)
+    @name = name
+
+    end
+
+  def add_student(student, grade)
+    @roster[grade] << [student]
+    # else
+    #   @roster[grade] << [student]
+    # end
+  end
+#
+#
+#
+#
+end
